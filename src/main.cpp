e6833cf930b300c09d400dd9ac2b00f16fdd9080
juanzb <juanzarache2000@gmail.com>
2025-10-15 05:43:51 -0500

    adaptation to changues about inicialization instance in Handle input/output

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <action_io.hpp>
 #include <config_io.hpp>
 #include <EEPROM.h>
 #include <serverHttp.hpp>
 #include <connect_wifi.hpp>
 #include <handleOutput.hpp>
+#include <handleInput.hpp>
 
 
 #define EEPROM_SIZE 12 // Tamaño de la EEPROM definida desde laEEPROM_ADDR_B00 hasta las EEPROM_ADDR_B11
@@ -20,25 +20,18 @@ void setup() {
   }
 
 void loop() {
-  
   HandleInput::loopInputs();
 
   if(stage == 0) {
     stage = 1;
-    Serial.println(HandleInput::instances.size());
-    HandleInput::instances.at(0)->setOutputIOShortPress(out_2);
-    HandleInput::instances.at(1)->setOutputIOShortPress(out_1);
-    HandleInput::instances.at(0)->setOutputIOLongPress(out_1);
-    HandleInput::instances.at(1)->setOutputIOLongPress(out_2);
+    Serial.println(HandleInput::getInstances().size());
+    HandleInput::getInstances().at(0)->setOutputIOShortPress(out_1);
+    HandleInput::getInstances().at(1)->setOutputIOShortPress(out_2);
+    HandleInput::getInstances().at(0)->setOutputIOLongPress(out_1);
+    HandleInput::getInstances().at(1)->setOutputIOLongPress(out_2);
     Serial.println("Start ");
     timeStart = millis();
   }
 
-  if (millis() - timeStart > 10000 && stage == 1) {
-    stage = 2;
-    HandleInput::instances.at(0)->setOutputIOShortPress(out_1);
-    HandleInput::instances.at(1)->setOutputIOShortPress(out_2);
-    Serial.println("Change IO");
-  }
 }
 
