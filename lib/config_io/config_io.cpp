6af236ed4764d9243f32c3ea0774c64ae7e2ab79
juanzb <juanzarache2000@gmail.com>
2025-10-27 06:12:49 -0500

    implement struct Input

--- lib/config_io/config_io.cpp
+++ lib/config_io/config_io.cpp
@@ -2,10 +2,10 @@
 #include <config_io.hpp>
 #include <struct_io.hpp>
 
-StrcInput in_1{GPIO_NUM_22, EEPROM_ADDR_B00};
-StrcInput in_2{GPIO_NUM_23, EEPROM_ADDR_B01};
-StrcOutput out_1{GPIO_NUM_5, EEPROM_ADDR_B02};
-StrcOutput out_2{GPIO_NUM_18, EEPROM_ADDR_B03};
+StrcInput in_1{GPIO_NUM_22, EEPROM_ADDR_B00, EEPROM_ADDR_B01};
+StrcInput in_2{GPIO_NUM_23, EEPROM_ADDR_B02, EEPROM_ADDR_B03};
+StrcOutput out_1{GPIO_NUM_5, EEPROM_ADDR_B04};
+StrcOutput out_2{GPIO_NUM_18, EEPROM_ADDR_B05};
 
 HandleInput i1(in_1);
 HandleInput i2(in_2);
