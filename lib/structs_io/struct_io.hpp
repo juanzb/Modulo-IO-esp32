0ace9668fb40671b6aa92642019d1a576d5230ff
juanzb <juanzarache2000@gmail.com>
2025-10-27 06:10:32 -0500

    adding address auxiliar to struct input

--- lib/structs_io/struct_io.hpp
+++ lib/structs_io/struct_io.hpp
@@ -28,19 +28,23 @@ struct StrcOutput {
  * 
  * @param pin Número del pin físico para input del microcontrolador.
  * @param addrEeprom Dirección en la EEPROM donde se almacena el byte de configuración de la entrada.
+ * @param addrEeprom Dirección en la EEPROM como AUX donde se almacena el byte de configuración de la entrada del puslo largo o corto.
  */
-  struct StrcInput {
+struct StrcInput {
   static const uint8_t ADDR_BIT_ENABLE_SP = 0;       // Número del bit para habilitar o deshabilitar la entrada.
   static const uint8_t ADDR_BIT_MODE_SP = 1;         // Número del bit para modo de funcionamiento del Short Press.
   static const uint8_t ADDR_BIT_VALUE_WRITE_SP = 2;  // Número del bit para valor a escribir al accionar con Short Press.
+  // static const uint8_t ADDRAUX_LOW_BITS_OUTPUT_SP = 1;   // Nivel del byte Auxiliar que representa la salida a controlar con Short Press.
   static const uint8_t ADDR_BIT_ENABLE_LP = 4;       // Número del bit para habilitar lógica de Long Press.
   static const uint8_t ADDR_BIT_MODE_LP = 5;         // Número del bit para modo de funcionamiento del Long Press.
   static const uint8_t ADDR_BIT_VALUE_WRITE_LP = 6;  // Número del bit para valor a escribir al accionar con Long Press.
-
+  // static const uint8_t ADDRAUX_HIGH_BITS_OUTPUT_LP = 2;   // Nivel del byte Auxiliar que representa la salida a controlar con Long Press.
+  
   uint8_t pin; 
   uint8_t addrEeprom;
-   
-  StrcInput(uint8_t _pin, uint8_t _addrEeprom)
-  : pin(_pin), addrEeprom(_addrEeprom)
+  uint8_t addrEepromAux; 
+
+  StrcInput(uint8_t _pin, uint8_t _addrEeprom, uint8_t _addrEepromAux)
+  : pin(_pin), addrEeprom(_addrEeprom), addrEepromAux(_addrEepromAux)
   {}
 };
