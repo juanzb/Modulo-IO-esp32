3493c6dd2c9ef1c628fb27f81c2fa92e4da632fb
juanzb <juanzarache2000@gmail.com>
2025-10-27 06:05:56 -0500

    improve handlebit, control directly to EEprom

--- lib/handleBit/handleBit.cpp
+++ lib/handleBit/handleBit.cpp
@@ -1,11 +1,43 @@
 #include <handleBit.hpp>
+#include <EEPROM.h>
 
-uint8_t handleBit::readBit(uint8_t &reg, uint8_t bit) {
+uint8_t handleBitEEprom::readBit(uint8_t &addr, uint8_t bit) {
+  uint8_t reg = EEPROM.read(addr);
   return (reg >> bit) & 0x01;
 }
 
-uint8_t handleBit::modifyBit(uint8_t &reg, uint8_t bit, uint8_t &value) {
+void handleBitEEprom::modifyBit(uint8_t &addr, uint8_t bit, uint8_t &value) {  
+  uint8_t reg = EEPROM.read(addr);
   reg &= ~(1 << bit);
   reg |= (value << bit);
-  return reg;
+  EEPROM.write(addr, reg);
+  EEPROM.commit();
+}
+
+uint8_t handleBitEEprom::readHighNibble(uint8_t addr) {
+  uint8_t value = EEPROM.read(addr);
+  return (value >> 4) & 0x0F;
+}
+
+uint8_t handleBitEEprom::readLowNibble(uint8_t addr) {
+  uint8_t value = EEPROM.read(addr);
+  return value & 0x0F;
+}
+
+void handleBitEEprom::writeHighNibble(uint8_t addr, uint8_t newHigh) {
+  uint8_t value = EEPROM.read(addr);
+  newHigh &= 0x0F;         // Asegura 4 bits
+  value &= 0x0F;           // Limpia nibble alto actual
+  value |= (newHigh << 4); // Inserta nuevo nibble alto
+  EEPROM.write(addr, value);
+  EEPROM.commit();
+}
+
+void handleBitEEprom::writeLowNibble(uint8_t addr, uint8_t newLow) {
+  uint8_t value = EEPROM.read(addr);
+  newLow &= 0x0F;
+  value &= 0xF0;           // Limpia nibble bajo actual
+  value |= newLow;         // Inserta nuevo nibble bajo
+  EEPROM.write(addr, value);
+  EEPROM.commit();
 }
--- lib/handleBit/handleBit.hpp
+++ lib/handleBit/handleBit.hpp
@@ -1,9 +1,21 @@
 #include <stdint.h>
 
-namespace handleBit {
+namespace handleBitEEprom {
   // registro, bit a leer 
-  uint8_t readBit(uint8_t &reg, uint8_t bit);
+  uint8_t readBit(uint8_t &addr, uint8_t bit);
   
   // registro, bit a modificar, valor
-  uint8_t modifyBit(uint8_t &reg, uint8_t bit, uint8_t &value);
+  void modifyBit(uint8_t &addr, uint8_t bit, uint8_t &value);
+
+  // Función para obtener la mitad alto
+  uint8_t readHighNibble(uint8_t value);
+
+  // Función para obtener la mitad bajo
+  uint8_t readLowNibble(uint8_t value);
+
+  // Función para modificar la mitad alto (0–15)
+  void writeHighNibble(uint8_t addr, uint8_t newHigh);
+
+  // Función para modificar la mitad bajo (0–15)
+  void writeLowNibble(uint8_t addr, uint8_t newLow);
 }
