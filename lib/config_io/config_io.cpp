df63f9df637184d30440d051a3f6073adccd9779
juanzb <juanzarache2000@gmail.com>
2025-10-27 12:25:34 -0500

    add funtion StartUpConfigIO

--- lib/config_io/config_io.cpp
+++ lib/config_io/config_io.cpp
@@ -2,13 +2,22 @@
 #include <config_io.hpp>
 #include <struct_io.hpp>
 
-StrcInput in_1{GPIO_NUM_22, EEPROM_ADDR_B00, EEPROM_ADDR_B01};
-StrcInput in_2{GPIO_NUM_23, EEPROM_ADDR_B02, EEPROM_ADDR_B03};
-StrcOutput out_1{GPIO_NUM_5, EEPROM_ADDR_B04};
-StrcOutput out_2{GPIO_NUM_18, EEPROM_ADDR_B05};
+void startUpConfigIO() {
+  static StrcInput in_1{GPIO_NUM_22, EEPROM_ADDR_B00, EEPROM_ADDR_B01};
+  static StrcInput in_2{GPIO_NUM_23, EEPROM_ADDR_B02, EEPROM_ADDR_B03};
+  static StrcOutput out_1{GPIO_NUM_5, EEPROM_ADDR_B04};
+  static StrcOutput out_2{GPIO_NUM_18, EEPROM_ADDR_B05};
 
-HandleInput i1(in_1);
-HandleInput i2(in_2);
+  static HandleOutput o1(out_1);
+  static HandleOutput o2(out_2);
+  static HandleInput i1(in_1);
+  static HandleInput i2(in_2);
 
-HandleOutput o1(out_1);
-HandleOutput o2(out_2);
+  HandleOutput::startUpOutputs();
+  HandleInput::startUpInputs();
+
+  Serial.print("INPUTS LOAD: ");
+  Serial.println(HandleInput::getInstances().size());
+  Serial.print("OUTPUTS LOAD: ");
+  Serial.println(HandleOutput::getInstances().size());
+}
--- lib/config_io/config_io.hpp
+++ lib/config_io/config_io.hpp
@@ -3,14 +3,4 @@
 #include <struct_io.hpp>
 #include <handleOutput.hpp>
 
-extern StrcInput in_1;
-extern StrcInput in_2;
-extern StrcOutput out_1;
-extern StrcOutput out_2;
-
-extern HandleInput i1;
-extern HandleInput i2;
-
-extern HandleOutput o1;
-extern HandleOutput o2;
-
+void startUpConfigIO();
