64266d0605ef66b16aafdcbf86be733081fd2b1a
juanzb <juanzarache2000@gmail.com>
2025-10-28 14:47:58 -0500

    adding output 3

--- lib/config_io/config_io.cpp
+++ lib/config_io/config_io.cpp
@@ -7,9 +7,11 @@ void startUpConfigIO() {
   static StrcInput in_2{GPIO_NUM_23, EEPROM_ADDR_B02, EEPROM_ADDR_B03};
   static StrcOutput out_1{GPIO_NUM_5, EEPROM_ADDR_B04};
   static StrcOutput out_2{GPIO_NUM_18, EEPROM_ADDR_B05};
+  static StrcOutput out_3{GPIO_NUM_2, EEPROM_ADDR_B06};
 
   static HandleOutput o1(out_1);
   static HandleOutput o2(out_2);
+  static HandleOutput o3(out_3);
   static HandleInput i1(in_1);
   static HandleInput i2(in_2);
 
